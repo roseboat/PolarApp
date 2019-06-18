85fe0e492542319b6386bbb6dcdd969b15735328
Rosa Downing <rosa.downing@SM-iMAC-0197.local>
2019-06-18 17:33:18 +0100

    small updates to iOS

---
+++ Polar.iOS/NewsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace Polar.iOS
+{
+    public partial class NewsController : UIViewController
+    {
+        public NewsController() : base("NewsController", null)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
